4a9cf2063ffebbb5b8831135e2a12addcaca2fd3
MohammadTausif7 <mohammedtausif64559@gmail.com>
2026-02-10 22:21:46 -0600

    File renaming

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import { motion } from "framer-motion";
-import Lamp from "../components/lamp.jsx";
+import Lamp from "../components/Lamp.jsx";
 import { logVisit } from "../lib/api.js";
 
 const NAME_KEY = "visitorName";
